c53a88053660975ec199532cdacb999f68400e65
Alriosa <carlos.alfredo.rios15@gmail.com>
2018-10-04 15:58:14 -0600

    WIP 1

---
+++ ColegioMVC/DataAccess/v1/Components/Entities-POJO/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities_POJO
+{
+    public class Student : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Last_Name { get; set; }
+
+
+    }
+}
